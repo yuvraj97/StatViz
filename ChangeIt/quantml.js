453dfbcfdbacc57db02c9a3b90137d54d0385319
Yuvraj Garg <yuvraj97.ml@gmail.com>
2020-12-17 18:56:21 +0530

    streamlit quantml*.js update

--- ChangeIt/quantml.js
+++ ChangeIt/quantml.js
@@ -10,14 +10,6 @@ meta.name = "author"
 meta.content = "Yuvraj Garg"
 document.getElementsByTagName('head')[0].appendChild(meta);
 
-/* <meta name="viewport" content="width=device-width, initial-scale=1" />
-<meta name="robots" content="index, follow">
-<meta name="description" content="Mission of QuantML is to make Machine Learning and Statistics Interactive. Here we can Visualize concepts in realtime, we also use programming-language to improve our understanding.">
-<meta name="keywords" content="QuantML,Visualization,Machine Learning,Statistics,Linear Algebra">
-<meta name="author" content="Yuvraj Garg"> */
-
-
-
 setTimeout(()=>{
 
     // Meta Tags
@@ -27,36 +19,56 @@ setTimeout(()=>{
     meta_description.name = "description"
 
     console.log("QUANTML")
-    document.querySelectorAll('.streamlit-expanderHeader').forEach(function(element, index){
-        element.style.borderBottomColor = "rgb(145, 145, 145)";
-        element.addEventListener("mouseover", function( event ) {
-            element.style.borderBottomColor = "#0073b1";
-        })
-        element.addEventListener('mouseleave', e => {
-            element.style.borderBottomColor = "rgb(145, 145, 145)";
-        })
-    })
-    document.querySelectorAll('.streamlit-expanderContent ').forEach(function(element, index){
-        element.style.borderBottomColor = "rgb(145, 145, 145)";
-    })
+    // document.querySelectorAll('.streamlit-expanderHeader').forEach(function(element, index){
+    //     element.style.borderBottomColor = "rgb(145, 145, 145)";
+    //     element.addEventListener("mouseover", function( event ) {
+    //         element.style.borderBottomColor = "#0073b1";
+    //     })
+    //     element.addEventListener('mouseleave', e => {
+    //         element.style.borderBottomColor = "rgb(145, 145, 145)";
+    //     })
+    // })
+    // document.querySelectorAll('.streamlit-expanderContent ').forEach(function(element, index){
+    //     element.style.borderBottomColor = "rgb(145, 145, 145)";
+    // })
 
 
     chapter = document.getElementById('quantml-chapter').innerHTML
     if(chapter == "wlln"){
-        console.log("current chapter: Weak Law of Large Numbers")
-        document.querySelector('title').innerHTML = "Weak Law of Large Numbers | Statistics App - Quantml"
+        // console.log("current chapter: Weak Law of Large Numbers")
+        // document.querySelector('title').innerHTML = "Weak Law of Large Numbers | Statistics App - Quantml"
         meta_keywords.content = "statistics,weak law of large numbers,visualization,weak law of large numbers visualization,law of large numbers,law of large numbers visualization,statistics app,quantml"
         meta_description.content = "Visualize Weak Law of Large Numbers | Statistics. See how changing parameters, distribution affects convergence. app.quantml.org here you can learn concepts by interacting with them."
     } else if (chapter == "clt"){
-        console.log("current chapter: Central Limit Theorem")
-        document.querySelector('title').innerHTML = "Central Limit Theorem | Statistics App - Quantml"
+        // console.log("current chapter: Central Limit Theorem")
+        // document.querySelector('title').innerHTML = "Central Limit Theorem | Statistics App - Quantml"
         meta_keywords.content = "statistics,central limit theorem,visualization,central limit theorem visualization,statistics app,quantml"
         meta_description.content = "Visualize Central Limit Theorem | Statistics. See how changing parameters, distribution affects convergence. app.quantml.org here you can learn concepts by interacting with them."
-    } else {
-        document.querySelector('title').innerHTML = "Statistics App - Quantml"
     }
 
     // Adding meta tags
     document.getElementsByTagName('head')[0].appendChild(meta_keywords);
     document.getElementsByTagName('head')[0].appendChild(meta_description);
 }, 2000)
+
+// quantmlCreateAttributeDiv("set-quantml-cookie", {name: "theme", value: "light"})
+
+body   = document.querySelector("body")
+const observer = new MutationObserver(function(mutations){
+    setTimeout(function(){
+        quantml_cookie_div   = document.getElementById("set-quantml-cookie")
+        if(quantml_cookie_div != null){
+            document.cookie = `${quantml_cookie_div.getAttribute("name")}=${quantml_cookie_div.getAttribute("value")}`
+            // quantml_cookie_div.remove()
+            console.log("Set Cookie2")
+        } else{
+            console.log("quantml_cookie not found2")    
+        }
+    }, 1000)
+})
+observer.observe(body, {
+    childList:true,
+    subtree:true
+})
+
+// div.remove()
