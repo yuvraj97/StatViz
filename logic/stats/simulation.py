ff9e718652035c8a66224b3ff917f924da317029
Yuvraj Garg <yuvraj97.ml@gmail.com>
2020-12-27 03:25:57 +0530

    Update simulation.py

--- logic/stats/simulation.py
+++ logic/stats/simulation.py
@@ -1,11 +1,17 @@
+from typing import Dict, Union
+
 import numpy as np
 import plotly.graph_objects as go
+from plotly.graph_objs import Figure
 
-def plot_binary_data(info, seed):
+
+def plot_binary_data(info: Dict[str, Union[str, int]], seed: Union[int, None]) -> Figure:
     if seed is not None: np.random.seed(seed)
+    count_1: int
+    count_2: int
     count_1, count_2 = info["count-1"], info["count-2"]
-    lim = max(count_1, count_2) * 2
-    fig = go.Figure()
+    lim: int = max(count_1, count_2) * 2
+    fig: Figure = go.Figure()
     fig.add_trace(go.Scatter(x=np.random.randint(0, lim, count_1),
                              y=np.random.randint(0, lim, count_1),
                              mode='markers',
