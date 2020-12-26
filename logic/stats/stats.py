be6dcae79a45e0739c09a614b6ca75aa58765631
Yuvraj Garg <yuvraj97.ml@gmail.com>
2020-12-27 03:23:47 +0530

    Update stats.py

--- logic/stats/stats.py
+++ logic/stats/stats.py
@@ -1,28 +1,38 @@
+from typing import Union, Tuple
 import numpy as np
 import plotly.express as px
+from plotly.graph_objs import Figure
 
-def get_plot(data, title):
+def get_plot(data: np.ndarray, title: str) -> Figure:
+    counts: np.ndarray
+    bins: np.ndarray
     counts, bins = np.histogram(np.array(data * 100, dtype=int), bins=range(0, 101, 2))
     bins = 0.5 * (bins[:-1] + bins[1:]) / 100
-    fig = px.bar(x=bins, y=counts, labels={'x': 'p', 'y': '# occurrence'})
+    fig: Figure = px.bar(x=bins, y=counts, labels={'x': 'p', 'y': '# occurrence'})
     fig.update_layout(title=title,
                       xaxis_title='Proportion of people turning their head to right (p)',
                       yaxis_title='# occurrence of certain (p) in our simulation')
     return fig
 
-def get_sample_with_true_p(population, p):
-    a = np.zeros(population, dtype=np.int8)
-    _1_s = int(population * p)
+def get_sample_with_true_p(population: int, p: float) -> np.ndarray:
+    a: np.ndarray = np.zeros(population, dtype=np.int8)
+    _1_s: int = int(population * p)
     for i in range(0, _1_s):
         a[i] = 1
     np.random.shuffle(a)
     return a
 
-def run(n_population, n_sample, true_p, n_simulations, title, seed):
+def run(n_population: int,
+        n_sample: int,
+        true_p: float,
+        n_simulations: int,
+        title: str,
+        seed: Union[int, None]) -> Tuple[np.ndarray, list, Figure, float, np.ndarray]:
+
     if seed is not None: np.random.seed(seed)
-    population_sample = get_sample_with_true_p(n_population, true_p)
-    estimators = np.empty(n_simulations)
-    samples = []
+    population_sample: np.ndarray = get_sample_with_true_p(n_population, true_p)
+    estimators: np.ndarray = np.empty(n_simulations)
+    samples: list = []
     for i in range(0, n_simulations):
         # Here we randomly select "n_sample" couples out of "population_sample"
         sample = np.random.choice(population_sample, n_sample)
