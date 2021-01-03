d7ad9eaff0a07f227d8d3c72b46753d0b47ad585
Yuvraj Garg <yuvraj97.ml@gmail.com>
2021-01-03 06:38:48 +0530

    [Created] auth.patreonAPI

--- auth/login.py
+++ auth/login.py
@@ -30,15 +30,42 @@ def initializeLogin(state, LOGIN_JSON, sidebar):
             st.markdown(f'<blockquote class="error">"<b>{email}</b>" is <b>not</b> a valid email address</blockquote>', unsafe_allow_html=True)
             return False, email
         elif email not in LOGIN_JSON:
-            msg = f"""
-                       <b>{email}</b> is not registered as a <b><a rel='noreferrer' target='_blank' href="https://www.patreon.com/quantml">patreon</a></b><br>
-                       This app is under development, once it's concluded it will be available to everyone.<br>
-                       To get <b>early access</b> to this app, <b>Join us at <a rel='noreferrer' target='_blank' href="https://www.patreon.com/quantml">patreon</a></b>
-                    """
-            st.markdown(f'<blockquote class="warning">{msg}</blockquote>', unsafe_allow_html=True)
+            from auth.patreonAPI import get_patreon_data
+            users = get_patreon_data()
+            if email in users:
+                st.markdown("""
+                <blockquote class="success">
+                    It's your first Login.<br>
+                    So let's Create a Password.
+                </blockquote>""", unsafe_allow_html=True)
+                LOGIN_JSON[email] = {
+                    "login": "false",
+                    "pass": "None",
+                    "OTP_COUNT": 0,
+                    "OTP_LIMIT_REACH_TIME": {
+                        "year": 0,
+                        "month": 0,
+                        "day": 0,
+                        "hour": 0,
+                        "minute": 0,
+                        "second": 0
+                    },
+                    "ID": "None"
+                }
+                resetPassword(state, email, LOGIN_JSON)
+            else:
+                msg = f"""
+                                       <b>{email}</b> is not registered as a <b><a rel='noreferrer' target='_blank' href="https://www.patreon.com/quantml">patreon</a></b><br>
+                                       This app is under development, once it's concluded it will be available to everyone.<br>
+                                       To get <b>early access</b> to this app, <b>Join us at <a rel='noreferrer' target='_blank' href="https://www.patreon.com/quantml">patreon</a></b>
+                                    """
+                st.markdown(f'<blockquote class="warning">{msg}</blockquote>', unsafe_allow_html=True)
         elif email in LOGIN_JSON:
             if LOGIN_JSON[email]['pass'] == "None":
-                st.header("Create Password")
+                st.markdown("""
+                <blockquote class="success>
+                    It's your first Login so let's Create a Password.
+                </blockquote>""", unsafe_allow_html=True)
                 resetPassword(state, email, LOGIN_JSON)
             else:
                 password = st.text_input("Password", type="password")
