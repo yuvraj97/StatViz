bedcd03653b4b82b8f1089c8a7ad7e210e27ab4c
Yuvraj Garg <yuvraj97.ml@gmail.com>
2021-09-06 16:20:27 +0530

    Move `Chapters/utils/SessionState.py` -> `utils/SessionState.py`

--- Chapters/Gaussian_Distribution/run.py
+++ Chapters/Gaussian_Distribution/run.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from Chapters.utils.SessionState import set_title
 from utils.ui import intialize, footer
 from utils.utils import set_get_URL
 from Chapters.Gaussian_Distribution import random_walk_1D, random_walk_2D
